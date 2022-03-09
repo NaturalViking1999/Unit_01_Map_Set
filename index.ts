bf5dafb3a95da8a72647352b217171e5e4781af6
Alexander <warrior0edem@gmail.com>
2022-03-09 19:22:37 +0300

    one

---
+++ index.ts
@@ -0,0 +1,82 @@
+interface IProtoData {
+    dt: string;
+    quantity: number;
+    age: number | null;
+    temp: {
+        temperature: string;
+        city: string;
+        optional?: boolean;
+    }
+}
+
+const arrDate: Array<IProtoData> = [
+    {
+        "dt": "01-01-2021",
+        "quantity": 100,
+        "age": null,
+        "temp": {
+            "temperature": "100 degrees",
+            "city": "Norway"
+        }
+    },
+    {
+        "dt": "02-01-2021",
+        "quantity": 150,
+        "age": 23,
+        "temp": {
+            "temperature": "103 degrees",
+            "city": "Egypt"
+        }
+    },
+    {
+        "dt": "03-01-2021",
+        "quantity": 150,
+        "age": 22,
+        "temp": {
+            "temperature": "98 degrees",
+            "city": "Kypris"
+        }
+    },
+    {
+        "dt": "03-01-2021",
+        "quantity": 150,
+        "age": 22,
+        "temp": {
+            "temperature": "98 degrees",
+            "city": "Holland"
+        }
+    },
+    {
+        "dt": "03-01-2021",
+        "quantity": 700,
+        "age": 27,
+        "temp": {
+            "temperature": "50 degrees",
+            "city": "Kursk",
+            "optional": true
+        }
+    },
+    {
+        "dt": "03-01-2021",
+        "quantity": 700,
+        "age": 27,
+        "temp": {
+            "temperature": "50 degrees",
+            "city": "Kursk",
+            "optional": true
+        }
+    }
+]
+
+let uniq = {}
+const newArrDate = arrDate.filter(obj => !uniq[obj.temp.city] && (uniq[obj.temp.city] = true));
+const dateSet = new Set(newArrDate);
+
+console.log(dateSet);
+
+const dateMap = new Map();
+newArrDate.forEach((obj, index) => {
+    dateMap.set(newArrDate[index].dt + ' ' + index, obj)
+});
+
+console.log(dateMap);
